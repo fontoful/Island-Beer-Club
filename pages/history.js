5ffa8cf42cb94862cf7c70a6dda8aa06293d1c1d
Hector Serrano <fonto.serrano@gmail.com>
2020-09-15 11:50:42 -0600

    Add history page (route)

---
+++ pages/history.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+import Header from '../components/header'
+
+const History = () => {
+	return (
+		<>
+			<Head>
+				<title>Island Beer Club | History</title>
+			</Head>
+			<Header />
+		</>
+	)
+}
+
+export default History
