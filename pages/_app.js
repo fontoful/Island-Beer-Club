7585e99ae81606894e76dbd24f289a02f7b449f9
Hector Serrano <fonto.serrano@gmail.com>
2020-09-15 11:53:05 -0600

    Import statements for styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import { config, library } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+
+// See https://github.com/FortAwesome/react-fontawesome#integrating-with-other-tools-and-frameworks
+// config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 
 export default MyApp
