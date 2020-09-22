6a0d29633debf7f1e9695604c7a41d3678349b0d
Hector Serrano <fonto.serrano@gmail.com>
2020-09-21 20:53:00 -0600

    Moved the local css import to be run after the bootstrap framework to fix the overriding of my styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
-import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 // import { config, library } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
+import '../styles/globals.css'
 
 // See https://github.com/FortAwesome/react-fontawesome#integrating-with-other-tools-and-frameworks
 // config.autoAddCss = false // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
