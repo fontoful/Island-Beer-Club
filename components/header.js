b8166ef752bdac8e7b1853f3c1d10529a15f7ccf
Hector Serrano <fonto.serrano@gmail.com>
2020-09-21 20:52:18 -0600

    Add Link wrapper to avoid client-side-rendering upon click

--- components/header.js
+++ components/header.js
@@ -16,27 +16,66 @@ const Header = () => (
 			<Navbar.Toggle aria-controls='basic-navbar-nav' />
 			<Navbar.Collapse id='basic-navbar-nav'>
 				<Nav className='navbar-nav ml-auto mr-5'>
-					<Nav.Link href='/history'>History</Nav.Link>
-					<Nav.Link href='/banner-travel'>Banner Travel</Nav.Link>
-					<NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-						<NavDropdown.Item href='/profiles'>
-							Profiles
-						</NavDropdown.Item>
-						<NavDropdown.Item href='/profiles-form'>
-							Profiles Form
-						</NavDropdown.Item>
-						<NavDropdown.Item href='/stats'>Stats</NavDropdown.Item>
+					<Link href='/history'>
+						<Nav.Link href='/history'>History</Nav.Link>
+					</Link>
+					<Link href='/banner-travel'>
+						<Nav.Link href='/banner-travel'>Banner Travel</Nav.Link>
+					</Link>
+					<NavDropdown
+						alignRight
+						title='More'
+						id='basic-nav-dropdown'
+					>
+						<Link href='/profiles'>
+							<NavDropdown.Item href='/profiles'>
+								Profiles
+							</NavDropdown.Item>
+						</Link>
+						<Link href='profiles-form'>
+							<NavDropdown.Item href='/profiles'>
+								Profiles Form
+							</NavDropdown.Item>
+						</Link>
+						<Link href='/stats'>
+							<NavDropdown.Item href='/stats'>
+								Stats
+							</NavDropdown.Item>
+						</Link>
 						<NavDropdown.Divider />
-						<NavDropdown.Item href='/styles'>
-							Styles
-						</NavDropdown.Item>
-						<NavDropdown.Item href='/ibus'>IBUs</NavDropdown.Item>
-						<NavDropdown.Item href='/links'>Links</NavDropdown.Item>
-						<NavDropdown.Item href='/pdfs'>PDFs</NavDropdown.Item>
-						<NavDropdown.Item href='/fun'>Fun</NavDropdown.Item>
-						<NavDropdown.Item href='/contact'>
-							Contact
-						</NavDropdown.Item>
+						<Link href='/founders-day'>
+							<NavDropdown.Item href='/founders-day'>
+								Founders Day
+							</NavDropdown.Item>
+						</Link>
+						<Link href='/styles'>
+							<NavDropdown.Item href='/styles'>
+								Styles
+							</NavDropdown.Item>
+						</Link>
+						<Link href='/ibus'>
+							<NavDropdown.Item href='/ibus'>
+								IBUs
+							</NavDropdown.Item>
+						</Link>
+						<Link href='/links'>
+							<NavDropdown.Item href='/links'>
+								Links
+							</NavDropdown.Item>
+						</Link>
+						<Link href='/pdfs'>
+							<NavDropdown.Item href='/pdfs'>
+								PDFs
+							</NavDropdown.Item>
+						</Link>
+						<Link href='/fun'>
+							<NavDropdown.Item href='/fun'>Fun</NavDropdown.Item>
+						</Link>
+						<Link href='/contact'>
+							<NavDropdown.Item href='/contact'>
+								Contact
+							</NavDropdown.Item>
+						</Link>
 					</NavDropdown>
 				</Nav>
 			</Navbar.Collapse>
