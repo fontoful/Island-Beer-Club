fa3e42020913a5505ebe6062f6381d9f1c202307
Hector Serrano <fonto.serrano@gmail.com>
2020-09-21 20:54:43 -0600

    Edited a few words

--- pages/history.js
+++ pages/history.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Header from '../components/header'
+import Footer from '../components/footer'
+import { Container, Row, Col, Button } from 'react-bootstrap'
 
 const History = () => {
 	return (
@@ -7,7 +9,183 @@ const History = () => {
 			<Head>
 				<title>Island Beer Club | History</title>
 			</Head>
+
 			<Header />
+
+			<Container>
+				<h2 class='text-center my-4'>Island Beer Club History</h2>
+				<strong>This page was last updated: 25 June 2020</strong>
+				<Row>
+					<Col lg={6}>
+						<p>
+							On a magnificent afternoon, mid-March in 2004, the
+							Island Beer Club (IBC) was founded. Jeff "Bones”
+							Wilkens (Mbr#1), John “Old Doc” Morton (Mbr#2), John
+							“Cruncher” Muncy (Mbr#3) and Wayne “Big Oly”
+							Oldendorph (Mbr#4) decided to sample hand-crafted,
+							full-bodied beers from local microbreweries in the
+							San Diego, California, area. After multiple
+							thirst-quenching stops, our four boys ended up at
+							the Ballast Point Brewing Company near the
+							University of San Diego campus. It was there that a
+							kegerator, on display for sale, caught the guys'
+							interest. The thought emerged… “Wouldn’t it be cool
+							if we could enjoy a cold, hand-crafted, micro-brewed
+							beer without the hassle of traveling off of the
+							"Rock?"
+							<br />
+							<br />
+							Since great ideas require intense thought and
+							discussion, our four-man brew crew adjourned to the
+							VFW bar in Coronado, CA for further liquid
+							refreshment. At the “V” the team agreed to form a
+							beer club. Bones would be the “capital” man--he
+							would purchase a kegerator and allow his garage to
+							be used for micro brew-beer-sampling sessions. Other
+							Club members would purchase kegs of beer, as
+							required. Having established the basic charter for
+							the club, the guys then adjourned to the Little Club
+							bar, in Coronado, to celebrate their brilliant
+							thoughts and begin the membership campaign.
+						</p>
+					</Col>
+					<Col lg={6}>
+						<p>
+							Our man of action, Bones, purchased the kegerator
+							just a few days later. And, on Monday, March 22,
+							2004, Old Doc delivered the first keg, Ballast Point
+							Yellowtail Pale Ale. Shortly thereafter, our
+							Original Four designated Rob "Crenny" Crenshaw Fifth
+							Founder, who joined in on the afternoon beer
+							sampling sessions for Coronado's new beer
+							aficionados. He became Member#5 after he purchased
+							his first keg.
+							<br />
+							<br />
+							Meeting at 4 pm, the five grew fond of watching
+							Jerry Springer (not much else on the tube at that
+							time) and discussing the happenings in the universe.
+							It was here that the boys developed the IBC
+							motto--“More Time than Money” -- to reflect the
+							retired status of most of the Beer Club's stately
+							members. The Founders also developed the club's
+							membership obligation: Buy a keg of hand-crafted
+							beer and you are a member. About ten months into our
+							new Club activities, we needed to solve a problem
+							for Jeff's spousal unit, Loie (aka Mrs. Bones). All
+							this time from inception, Club members were allowed
+							to visit the restroom facilities in the house.
+							Unfortunately, those waiting outside for the
+							facility to "clear" would relieve themselves in
+							Loie's garden... This was intolerable.
+						</p>
+					</Col>
+					<Col lg={12}>
+						<p>
+							On the first anniversary of the Beer Club, the first
+							urinal was installed. In 2013 the original urinal
+							was retired and replaced with a waterless urinal.
+							Unfortunately, nowadays, our meeting location may
+							not offer a lady's restroom.
+							<br />
+							<br />
+							Always alert and "forward observant" (looking to the
+							future), Bones purchased the domain name,
+							www.islandbeerclub.com. He knew that it might be
+							worthwhile, but he had no idea what to do with it or
+							how it would be helpful... so we sat on it until
+							July of 2006. At this point, Crenny had gained some
+							experience in website development and was hosting
+							the website for the Sea 'N Air Men's Golf Club. That
+							was all we needed. A few pages were developed, and
+							the IBC website was officially published in July
+							2006. This award-winning website is the envy of beer
+							clubs everywhere.
+							<br />
+							<br />
+							Signage plays a big part at the Island Beer Club. In
+							August 2006, our alley became "Beer Belly Blvd." A
+							year later a permanent brass marker was installed
+							commemorating the site of the IBC Headquarters.
+							October 2007 had John Clampitt (Mbr#18) deliver and
+							install a hand-crafted, kiln-fired tile plaque to
+							identify the clubhouse of the IBC.
+							<br />
+							<br />
+							John Weiman (Mbr#151), Weiman Design, designed our
+							logo in late October 2007; allowing us to acquire
+							our now highly sought after, custom apparel and
+							glassware.
+							<br />
+							<br />
+							In our first fourteen years of existence, we have
+							continually improved and upgraded the facility to
+							include a commercial popcorn maker (compliments of
+							Mike Herlihy (Mbr#57)), two additional beer
+							kegerators (thanks Bones (Mbr#1)), a granite wash
+							sink (thanks to IBC plumber Bob Ryan (Mbr#120) and
+							granite donator Ed Carney (Mbr#58)), two
+							flame-thrower outdoor heaters and numerous
+							beer-oriented signs. Also, the Island Beer Club
+							banner has been on many photo ops including the top
+							of Kilimanjaro, 2012 Super Bowl and, a $16 million
+							cocaine bust off Costa Rica. Between, 2011 and 2018,
+							we more than tripled our membership as we focused on
+							recruiting beer aficionados with enough disposable
+							income to provide a keg of hand-crafted beer. As of
+							June 2020, we have 407 members.
+							<br />
+							<br />
+							Every March, on the Wednesday closest to 22 March,
+							we celebrate the anniversary of the Island Beer Club
+							by holding a Founders Day party. This event honors
+							the five Founders for creating Coronado’s favorite
+							weekly alley beer gathering.
+							<br />
+							<br />
+							We lost Jeffery “Bones” Wilkens, (Mbr#1), on 6 Jul
+							2019. On 7 Aug 2019, a huge turnout joined together
+							in Beer Belly Blvd to recognize Jeff, celebrate his
+							life and salute his passion for and contribution to
+							the Island Beer Club. On 4 Sep 2019 IBC
+							Headquarters, at 851 J Ave, was retired and on the
+							same day, we celebrated the Grand Opening of the IBC
+							Clubhouse next door at 857 J Ave, Rod Neilson’s (Mbr
+							#169) house.
+							<br />
+							<br />
+							The J Ave Clubhouse now hosts Beer Club sessions at
+							least twice a month. Other Beer Club sessions are at
+							our local venues, VFW Coronado, The Little Club,
+							Attitude Brewing Co, and Feast and Fareway
+							restaurant at the Coronado Golf Course. On 18 Mar
+							2020, because of COVID-19 precautions, Beer Club
+							halted weekly meetings until further notice. As of
+							23 Jun 2020, no IBC full-membership meeting has been
+							conducted at the Clubhouse.
+							<br />
+							<br />
+							Our first order of business when we resume
+							alley-socializing is to celebrate Founder’s Day
+							2020.
+							<br />
+							<br />
+							The IBC meets every Wednesday from 4 PM to 6 PM
+							(Standard Time); 4:30 PM to 6:30 PM (Daylight
+							Savings Time) in the alley (Beer Belly Blvd) on J
+							Avenue in Coronado as well as our “satellite”
+							locations. Guests, especially ladies, are always
+							welcome when accompanied by a Beer Club member.
+							Please join us.
+						</p>
+
+						<br />
+						<br />
+					</Col>
+				</Row>
+			</Container>
+
+			<Footer />
 		</>
 	)
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,17 @@
 import Head from 'next/head'
 import Header from '../components/header'
-import { Container, Row, Col, Button, Dropdown } from 'react-bootstrap'
+import Footer from '../components/footer'
+import { Container, Row, Col, Dropdown } from 'react-bootstrap'
+import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
 
 export default function Home() {
 	return (
 		<>
 			<Head>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Poppins&display=swap'
+					rel='stylesheet'
+				/>
 				<title>Island Beer Club</title>
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
@@ -14,8 +20,8 @@ export default function Home() {
 
 			{/* Main section */}
 			<Container>
-				<Row>
-					<Col lg={6} className='text-center p5 mt-4'>
+				<Row className='mb-5'>
+					<Col lg={6} className='text-center p5 my-4'>
 						<div className='opening-text mt-3'>
 							<p>
 								We are a bunch of guys who grew up together and
@@ -36,25 +42,30 @@ export default function Home() {
 							<button type='button' className='btn btn-info'>
 								Sign up for our IBC eGram
 							</button>
-							<div className='dropdown-divider'></div>
 						</div>
 					</Col>
 
 					<Col lg={6} className='mt-4'>
-						<div class='YouTube-video mt-4'>
+						<div className='YouTube-video mt-4'>
 							<p>
 								We ARE the Island Beer Club - Founded in 2004
 								CLICK on the picture below to begin "IBC... The
 								Movie."
 							</p>
-							<iframe
+							{/* <iframe
 								width='560'
 								height='315'
 								src='https://www.youtube.com/embed/5q_aWSWDn94'
 								frameborder='0'
 								allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
 								allowfullscreen
-							></iframe>
+							></iframe> */}
+							<ResponsiveEmbed aspectRatio='16by9'>
+								<embed
+									src='https://www.youtube.com/embed/5q_aWSWDn94'
+									// type='video'
+								/>
+							</ResponsiveEmbed>
 						</div>
 					</Col>
 				</Row>
@@ -62,7 +73,7 @@ export default function Home() {
 
 			<section className='disclaimer mt-4'>
 				<Container>
-					<Row>
+					<Row className='mb-4'>
 						<Col className='text-center mt5 mb4'>
 							<h3>Remember, RULE #1: There ARE no rules.</h3>
 							<p>
@@ -72,7 +83,192 @@ export default function Home() {
 						</Col>
 					</Row>
 				</Container>
+				<Container>
+					<Row>
+						<Col>
+							<strong>
+								<p>
+									Please take a tour. We will be adding more
+									areas as we grow.
+								</p>
+							</strong>
+							<ul>
+								<li>
+									<a href='https://www.kegworks.com/blog/guide-to-beer-keg-sizes/'>
+										How many pints in a half barrell?
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										Want help choosing your next Beer?
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										We care about your safety. Check out our
+										Notification page
+									</a>
+								</li>
+								<li>
+									<a href='#'>
+										Proud to be an American? Pass this link
+										around
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										Meet our members. Watch our growth
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										Know anything about Coronado and Some
+										Like It Hot?
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										The Poem Stranger has blessed us with a
+										poem.
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										Know anything about beer styles?
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										How much does beer cost world-wide?
+									</a>
+								</li>
+								<li>
+									<a href=''>How bitter is your beer?</a>
+								</li>
+								<li>
+									<a href=''>IBC Science Corner.</a>
+								</li>
+								<li>
+									<a href=''>Check your biorythmns.</a>
+								</li>
+								<li>
+									<a href=''>
+										How long has it been since you've played
+										Hangman?
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										What's the oldest brewery in the U. S.?
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										How much beer has the IBC consumed?
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										Do you know anything about Foo Dogs?
+									</a>
+								</li>
+								<li>
+									<a href=''>
+										How much do you know about brewing?
+									</a>
+								</li>
+							</ul>
+						</Col>
+					</Row>
+				</Container>
 			</section>
+
+			<hr className='my-4' style={{ width: '60%', margin: 'auto' }} />
+
+			<section className='phrases'>
+				<Container>
+					<Row>
+						<Col>
+							<h3>Best Club on this side of the drink!</h3>
+							<small>Ken May, 30 Oct 09</small>
+						</Col>
+					</Row>
+					<Row>
+						<Col className='mb-4'>
+							<p>
+								Regular consumption of beer eliminates the
+								weaker brain cells, making the brain a faster
+								and more efficient machine. That's why you
+								always feel smarter after a few beers.
+							</p>
+							<small>
+								-- Cliff Clavin (Submitted by Richard Smith, 24
+								Mar 10)
+							</small>
+						</Col>
+					</Row>
+
+					<hr className='mt-3 mb-3' />
+					<Row className='mb-3'>
+						<Col>
+							<strong>ALWAYS PLAN AHEAD!</strong> -- Buy two cases
+							of beer
+						</Col>
+					</Row>
+					<Row>
+						<Col sm={6}>
+							<p>
+								Beer is proof that God loves us and wants us to
+								be happy. Benjamin Franklin
+							</p>
+						</Col>
+						<Col sm={6}>
+							<p>
+								Beer is not just a breakfast beverage. It is a
+								way of life. COL Jack Holly, USMC
+							</p>
+						</Col>
+						<Col sm={6}>
+							<p>
+								Finish your beer. There are sober folks in
+								China. Unknown
+							</p>
+						</Col>
+						<Col sm={6}>
+							<p>
+								Life's tough... it's even tougher if you're
+								stupid. John Wayne
+							</p>
+						</Col>
+						<Col sm={6}>
+							<p>
+								I read about the evils of drinking, I gave up
+								reading. Henny Youngman
+							</p>
+						</Col>
+						<Col sm={6}>
+							<p>
+								Everybody's got to believe in something. I
+								believe I'll have another beer. W. C. Fields
+							</p>
+						</Col>
+						<Col sm={6}>
+							<p>
+								Alcohol - the cause of and the solution to - all
+								of life's problems. Homer Simpson
+							</p>
+						</Col>
+						<Col sm={6}>
+							<p>
+								Beer: Helping ugly people have sex since 1862.
+								Anonymous
+							</p>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+
+			<Footer />
 		</>
 	)
 }
