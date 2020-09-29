3ba69f8af5b989117120477ef21ab71c397a4541
Hector Serrano <fonto.serrano@gmail.com>
2020-09-29 00:07:05 -0600

    Add bare minimum for new pages

---
+++ pages/style.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Header from '../components/header'
+import Footer from '../components/footer'
+
+const Styles = () => {
+	return (
+		<>
+			<Head>
+				<link
+					href='https://fonts.googleapis.com/css2?family=Poppins&display=swap'
+					rel='stylesheet'
+				/>
+				<title>Island Beer Club</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<Header />
+
+			<Footer />
+		</>
+	)
+}
+
+export default Styles
