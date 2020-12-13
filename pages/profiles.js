70d1a03198e3402ea85aa4508855e4c732a1c8a0
Hector Serrano <fonto.serrano@gmail.com>
2020-12-12 21:32:34 -0600

    profiles.js refactoring

--- pages/profiles.js
+++ pages/profiles.js
@@ -27,6 +27,7 @@ const getUrl = pathToFile => {
 const Profiles = props => {
 	const router = useRouter()
 	const [profiles, setProfiles] = useState(props.profiles)
+
 	const handleFormSubmit = async e => {
 		e.preventDefault()
 
@@ -40,11 +41,13 @@ const Profiles = props => {
 		if (searchInput !== '') {
 			query.search = searchInput
 		}
+
 		router.push({
 			pathname: router.pathname,
 			query,
 		})
 	}
+
 	return (
 		<>
 			<Head>
@@ -56,7 +59,10 @@ const Profiles = props => {
 					<p className='display-2'>Member Profiles</p>
 				</Jumbotron>
 				<div className='profile-search__container'>
-					<Form className='profile-search__form' onSubmit={handleFormSubmit}>
+					<Form
+						className='profile-search__form'
+						onSubmit={handleFormSubmit}
+					>
 						<input
 							className='profile-search__input'
 							type='text'
@@ -64,47 +70,85 @@ const Profiles = props => {
 							placeholder='Search by name...'
 							defaultValue={router.query.search}
 						/>
-						<Button type='submit' size='xxl' >Search</Button>
+						<Button type='submit' size='xxl'>
+							Search
+						</Button>
 					</Form>
 				</div>
 				<div className='profile__container'>
-						{profiles.map((profile) => (
-								<div key={profile.mbr} className='profile-card shadow'>
-										
-									<div className='profile-header'>
-										<Image className='profile-img' thumbnail src={getUrl(profile.img)} />
-										<div className='profile-stats text-light'>
-											<h4>{profile.name} </h4>
-											<h5>MSD: {profile.msd}</h5>
-											<h6>Sponsor: {profile.sponsor}</h6>
-										</div>
-										<div className='profile-number pt-2 text-light'>
-											<h3>#{profile.mbr}</h3>
-										</div>
-									</div>
-									<div className='profile-navigation'>
-										<Tabs defaultActiveKey="bio" id="uncontrolled-tab-example">
-											<Tab eventKey="bio" title="Bio" className='profile-body'>
-											{profile.bio.length === 0 ? 'No bio.' : profile.bio}
-											</Tab>
-											<Tab eventKey="likes" title="Likes" className='profile-body'>  
-											{profile.likes.length === 0 ? 'No likes.' : profile.likes}
-											</Tab>
-											<Tab eventKey="dislikes" title="Dislikes" className='profile-body'>  
-											{profile.dislikes.length === 0 ? 'No dislikes.' : profile.dislikes}
-											</Tab>
-											<Tab eventKey="hobbies" title="Hobbies" className='profile-body'>  
-											{profile.hobbies.length === 0 ? 'No hobbies.' : profile.hobbies}
-											</Tab>
-											<Tab eventKey="nicknames" title="Nicknames" className='profile-body'>
-											{profile.nicknames.length === 0 ? 'No nicknames.' : profile.nicknames}
-											</Tab>
-										</Tabs>
-									</div>
+					{profiles.map(profile => (
+						<div key={profile.mbr} className='profile-card shadow'>
+							<div className='profile-header'>
+								<Image
+									className='profile-img'
+									thumbnail
+									src={getUrl(profile.img)}
+								/>
+								<div className='profile-stats text-light'>
+									<h4>{profile.name} </h4>
+									<h5>MSD: {profile.msd}</h5>
+									<h6>Sponsor: {profile.sponsor}</h6>
 								</div>
-						))}
+								<div className='profile-number pt-2 text-light'>
+									<h3>#{profile.mbr}</h3>
+								</div>
+							</div>
+							<div className='profile-navigation'>
+								<Tabs
+									defaultActiveKey='bio'
+									id='uncontrolled-tab-example'
+								>
+									<Tab
+										eventKey='bio'
+										title='Bio'
+										className='profile-body'
+									>
+										{profile.bio.length === 0
+											? 'No bio.'
+											: profile.bio}
+									</Tab>
+									<Tab
+										eventKey='likes'
+										title='Likes'
+										className='profile-body'
+									>
+										{profile.likes.length === 0
+											? 'No likes.'
+											: profile.likes}
+									</Tab>
+									<Tab
+										eventKey='dislikes'
+										title='Dislikes'
+										className='profile-body'
+									>
+										{profile.dislikes.length === 0
+											? 'No dislikes.'
+											: profile.dislikes}
+									</Tab>
+									<Tab
+										eventKey='hobbies'
+										title='Hobbies'
+										className='profile-body'
+									>
+										{profile.hobbies.length === 0
+											? 'No hobbies.'
+											: profile.hobbies}
+									</Tab>
+									<Tab
+										eventKey='nicknames'
+										title='Nicknames'
+										className='profile-body'
+									>
+										{profile.nicknames.length === 0
+											? 'No nicknames.'
+											: profile.nicknames}
+									</Tab>
+								</Tabs>
+							</div>
+						</div>
+					))}
 				</div>
-				<Footer/>
+				<Footer />
 			</Container>
 		</>
 	)
