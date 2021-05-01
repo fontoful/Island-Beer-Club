93cf39e9a916255492e9dbee1bedb4eabcf0a89f
Hector Serrano <fonto.serrano@gmail.com>
2021-05-01 16:04:01 -0600

    Modify collection to user-data

--- pages/api/profiles.js
+++ pages/api/profiles.js
@@ -1,38 +1,36 @@
 import firebase from '../../lib/firebase'
 
 export const loadProfiles = async (query = {}) => {
-	const { search } = query
+  const { search } = query
 
-	const numbSearch = Number.isNaN(+search)
+  const numbSearch = Number.isNaN(+search)
 
-	let docsSnap = await firebase
-		.firestore()
-		.collection('profilesTest')
-		//.where('is_member', '==', 'true')
-		.orderBy('mbr', 'asc')
-		.get()
+  let docsSnap = await firebase
+    .firestore()
+    .collection('user-data')
+    //.where('is_member', '==', 'true')
+    .orderBy('mbr', 'asc')
+    .get()
 
-	let profiles = docsSnap.docs.map(doc => doc.data())
+  let profiles = docsSnap.docs.map(doc => doc.data())
 
-	if (search) {
-		if (numbSearch) {
-			const searchRegex = new RegExp(search, 'gi')
-			profiles = profiles.filter(profile =>
-				profile.name.match(searchRegex),
-			)
-		} else {
-			profiles = profiles.filter(profile => profile.mbr === +search)
-		}
-	}
+  if (search) {
+    if (numbSearch) {
+      const searchRegex = new RegExp(search, 'gi')
+      profiles = profiles.filter(profile => profile.name.match(searchRegex))
+    } else {
+      profiles = profiles.filter(profile => profile.mbr === +search)
+    }
+  }
 
-	return profiles
+  return profiles
 }
 
 export default async (req, res) => {
-	const output = {
-		profiles: await loadProfiles(req.query),
-	}
+  const output = {
+    profiles: await loadProfiles(req.query),
+  }
 
-	res.setHeader('Content-Type', 'application/json')
-	res.end(JSON.stringify(output))
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(output))
 }
